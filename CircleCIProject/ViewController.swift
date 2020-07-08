ebc84bcd145bbec45a2dc997621e60fafb24bcfb
Jose <josman185@gmail.com>
2020-07-07 20:38:02 -0500

    Initial Commit

---
+++ CircleCIProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleCIProject
+//
+//  Created by Jose Vargas on 07/07/20.
+//  Copyright © 2020 joscompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
